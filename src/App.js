c705fd6e9e3fe061a4836015f0a099178aef72f0
rndrouin <rob.drouin@gmail.com>
2024-03-06 13:27:19 -0500

    This is pushing the Optional 1 refactor with components in the same app.js file

--- src/App.js
+++ src/App.js
@@ -4,6 +4,95 @@ import './App.css';
 
 function log(message){console.log(message);}
 
+function CustomerList({customers, selectedRow, handleListClick}) {
+  return (
+  <div className="boxed" >
+  <h4>Customer List</h4>
+  <table id="customer-list">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Password</th>
+      </tr>
+    </thead>
+    <tbody>
+      {customers.map(
+        (item, index) => {
+          return (<tr key={item.id} 
+            className={selectedRow === item.id ? 'selected' : ''} // RD - Step 9 - Bold List Item - Add 'selected' class if row is selected
+          onClick={()=>handleListClick(item)} 
+          >
+            <td>{item.name}</td>
+            <td>{item.email}</td>
+            <td>{item.password}</td>
+          </tr>);
+        }
+      )}
+    </tbody>
+  </table>
+</div>
+  );
+}
+
+function CustomerAddUpdateForm({
+  formObject,
+  handleInputChange,
+  onDeleteClick,
+  onSaveClick,
+  onCancelClick,
+  mode,
+}) {
+  return (
+    <div className="boxed">
+    <div>
+      <h4>{mode}</h4>
+    </div>
+    <form >
+      <table id="customer-add-update" >
+        <tbody>
+          <tr>
+            <td className={'label'} >Name:</td>
+            <td><input
+              type="text"
+              name="name"
+              onChange={(e) => handleInputChange(e)}
+              value={formObject.name}
+              placeholder="Customer Name"
+              required /></td>
+          </tr>
+          <tr>
+            <td className={'label'} >Email:</td>
+            <td><input
+              type="email"
+              name="email"
+              onChange={(e) => handleInputChange(e)}
+              value={formObject.email}
+              placeholder="name@company.com" /></td>
+          </tr>
+          <tr>
+            <td className={'label'} >Pass:</td>
+            <td><input
+              type="text"
+              name="password"
+              onChange={(e) => handleInputChange(e)}
+              value={formObject.password}
+              placeholder="password" /></td>
+          </tr>
+          <tr className="button-bar">
+            <td colSpan="2">
+              <input type="button" value="Delete" onClick={onDeleteClick} />
+              <input type="button" value="Save" onClick={onSaveClick} />
+              <input type="button" value="Cancel" onClick={onCancelClick} />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </form>
+  </div>
+  );
+}
+
 export function App(params) {
   let blankCustomer = { "id": -1, "name": "", "email": "", "password": "" };
   //let formObject = customers[0];
@@ -59,87 +148,28 @@ export function App(params) {
   let onSaveClick = function () {
     if (mode === 'Add') {
       post(formObject);
-}
-if (mode === 'Update') {
-put(formObject.id, formObject);
-}
-setFormObject(blankCustomer);
+    }
+    if (mode === 'Update') {
+      put(formObject.id, formObject);
+    }
+    setFormObject(blankCustomer);
   }
-
+  
   return (
     <div>
-      <div className="boxed" >
-        <h4>Customer List</h4>
-        <table id="customer-list">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Password</th>
-            </tr>
-          </thead>
-          <tbody>
-            {customers.map(
-              (item, index) => {
-                return (<tr key={item.id} 
-                  className={selectedRow === item.id ? 'selected' : ''} // RD - Step 9 - Bold List Item - Add 'selected' class if row is selected
-                onClick={()=>handleListClick(item)} 
-                >
-                  <td>{item.name}</td>
-                  <td>{item.email}</td>
-                  <td>{item.password}</td>
-                </tr>);
-              }
-            )}
-          </tbody>
-        </table>
-    </div>
-    <div className="boxed">
-      <div>
-        <h4>{mode}</h4>
-      </div>
-      <form >
-        <table id="customer-add-update" >
-          <tbody>
-            <tr>
-              <td className={'label'} >Name:</td>
-              <td><input
-                type="text"
-                name="name"
-                onChange={(e) => handleInputChange(e)}
-                value={formObject.name}
-                placeholder="Customer Name"
-                required /></td>
-            </tr>
-            <tr>
-              <td className={'label'} >Email:</td>
-              <td><input
-                type="email"
-                name="email"
-                onChange={(e) => handleInputChange(e)}
-                value={formObject.email}
-                placeholder="name@company.com" /></td>
-            </tr>
-            <tr>
-              <td className={'label'} >Pass:</td>
-              <td><input
-                type="text"
-                name="password"
-                onChange={(e) => handleInputChange(e)}
-                value={formObject.password}
-                placeholder="password" /></td>
-            </tr>
-            <tr className="button-bar">
-              <td colSpan="2">
-                <input type="button" value="Delete" onClick={onDeleteClick} />
-                <input type="button" value="Save" onClick={onSaveClick} />
-                <input type="button" value="Cancel" onClick={onCancelClick} />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </form>
-    </div>
+      <CustomerList
+        customers={customers}
+        selectedRow={selectedRow}
+        handleListClick={handleListClick}
+      />
+      <CustomerAddUpdateForm
+        formObject={formObject}
+        handleInputChange={handleInputChange}
+        onDeleteClick={onDeleteClick}
+        onSaveClick={onSaveClick}
+        onCancelClick={onCancelClick}
+        mode={mode}
+      />
     </div>
   );
 }
